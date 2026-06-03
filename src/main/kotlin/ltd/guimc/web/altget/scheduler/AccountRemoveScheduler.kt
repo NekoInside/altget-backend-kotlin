64a808c88b9d0721f12ace3de580b63654fd2960
guimc <xiluo@guimc.ltd>
2026-06-04 00:36:06 +0800

    fix: filter condition is backwards

--- src/main/kotlin/ltd/guimc/web/altget/scheduler/AccountRemoveScheduler.kt
+++ src/main/kotlin/ltd/guimc/web/altget/scheduler/AccountRemoveScheduler.kt
@@ -3,6 +3,7 @@ package ltd.guimc.web.altget.scheduler
 import ltd.guimc.web.altget.enum.EnumUserRole
 import ltd.guimc.web.altget.service.user.CoreAuthService
 import ltd.guimc.web.altget.service.user.UserDetailsService
+import org.slf4j.LoggerFactory
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 import java.time.LocalDateTime
@@ -13,12 +14,21 @@ class AccountRemoveScheduler(
     private val userDetailsService: UserDetailsService,
     private val coreAuthService: CoreAuthService
 ) {
+    private val logger = LoggerFactory.getLogger(AccountRemoveScheduler::class.java)
+
     @Scheduled(fixedDelay = 10, timeUnit = TimeUnit.MINUTES)
     fun removeScheduler() {
+        logger.debug("AccountRemoveScheduler triggered")
         val userDetails = userDetailsService.list()
-        userDetails.filter {
-            it.userRole == EnumUserRole.UNVERIFY && it.registerTime.minusMinutes(10) > LocalDateTime.now()
-        }.forEach {
+        val toRemove = userDetails.filter {
+            it.userRole == EnumUserRole.UNVERIFY && it.registerTime.isBefore(LocalDateTime.now().minusMinutes(10))
+        }
+        if (toRemove.isEmpty()) {
+            logger.debug("No unverified accounts to remove")
+            return
+        }
+        logger.info("Removing {} unverified accounts", toRemove.size)
+        toRemove.forEach {
             coreAuthService.removeAccount(it.userId!!)
         }
     }
--- src/main/kotlin/ltd/guimc/web/altget/scheduler/AltConsumptionCleanupScheduler.kt
+++ src/main/kotlin/ltd/guimc/web/altget/scheduler/AltConsumptionCleanupScheduler.kt
@@ -1,6 +1,7 @@
 package ltd.guimc.web.altget.scheduler
 
 import ltd.guimc.web.altget.service.alt.AltConsumptionRecordService
+import org.slf4j.LoggerFactory
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 import java.util.concurrent.TimeUnit
@@ -9,8 +10,12 @@ import java.util.concurrent.TimeUnit
 class AltConsumptionCleanupScheduler(
     private val altConsumptionRecordService: AltConsumptionRecordService
 ) {
+    private val logger = LoggerFactory.getLogger(AltConsumptionCleanupScheduler::class.java)
+
     @Scheduled(fixedDelay = 1, timeUnit = TimeUnit.HOURS)
     fun cleanupOldConsumptionRecords() {
-        altConsumptionRecordService.cleanupOldRecords(30)
+        logger.debug("AltConsumptionCleanupScheduler triggered")
+        val deleted = altConsumptionRecordService.cleanupOldRecords(30)
+        logger.debug("Cleanup finished, {} records deleted", deleted)
     }
 }
